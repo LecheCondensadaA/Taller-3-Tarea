64550ab27122c2a06dde64713ef156015ee94316
DonPerip <pixel.stars.p@gmail.com>
2025-05-03 17:49:03 -0400

    Update experimentacion.cpp

--- Codigo-Fuente/src/experimentacion.cpp
+++ Codigo-Fuente/src/experimentacion.cpp
@@ -1,2 +1,9 @@
 #include "experimentacion.h"
 #include "vector.h"
+#include <chrono> // ocupa los comandos para leer el codigo
+using namespace std;
+
+// chrono::high_resolution_clock::time_point nombre_de_variable = chrono::high_resolution_clock::now();
+// este deberia ser el codigo que deberiamos usar para leer el codigo
+// nesecitando una variable para tomar el valor inicial, y otra para el valor final
+
