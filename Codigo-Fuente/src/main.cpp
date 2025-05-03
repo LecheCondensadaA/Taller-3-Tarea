ba0656e191e31e2f7a0c25c713556574341fd9b5
DonPerip <pixel.stars.p@gmail.com>
2025-05-03 18:02:37 -0400

    a
    avance en el main
    y mas comentarios

--- Codigo-Fuente/src/main.cpp
+++ Codigo-Fuente/src/main.cpp
@@ -1,5 +1,10 @@
 #include "experimentacion.h"
+#include<iostream>
+using namespace std;
 
-int main(){
-    return 0;
-}
+int main(int argc, char **argv){
+    if(argc != 2){
+	    cout << "Error. Debe ejecutarse como ./problema k" << endl;
+	    exit(EXIT_FAILURE);
+    }
+
--- Codigo-Fuente/src/vector.cpp
+++ Codigo-Fuente/src/vector.cpp
@@ -1,1 +1,2 @@
 #include "vector.h"
+#include<filesystem> // la libreria para poder ver y leer los archivos que hay en el directorio
