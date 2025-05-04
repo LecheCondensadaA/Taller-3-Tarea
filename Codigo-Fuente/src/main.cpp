3892eb15618466ab7303ad1f15c9251f62b660ab
DonPerip <pixel.stars.p@gmail.com>
2025-05-03 20:38:04 -0400

    Update main.cpp

--- Codigo-Fuente/src/main.cpp
+++ Codigo-Fuente/src/main.cpp
@@ -2,53 +2,46 @@
 #include <vector>
 #include <filesystem>
 #include <string>
-#include <chrono>
-
 using namespace std;
 namespace fs = std::filesystem;
 using namespace fs;
 #include <iostream>
+#include <chrono>
 
 int main(int argc, char** argv){
     if(argc != 2){
-        cout << "Error. Debe ejecutarse como ./tarea k" << endl;
+        cout << "Error. Debe ejecutarse como ./tarea directorio" << endl;
         exit(EXIT_FAILURE);
     }
-    int num_files=atoi(argv[1]);
-    int i;
-    string name, ext;
-    path dir1 = "/home/flowerpot-vt/Progra/Tarea-INFO088-2025S1/direc";
-    if (!exists(dir1)) { 
-
-    }
-    else{
-        cout << "papu papu, existe el papu directorio papu" <<endl;
-    }
+    path dir1 = argv[1];
+      
+    //Creacion del vector:
     cout << "Creacion del vector..." <<endl;
-    chrono::high_resolution_clock::time_point ini = chrono::high_resolution_clock::now();
-    vector <string> v(num_files);
+    vector <string> v; //Crea un vector vacio.
+    
+    //Se checa si existe el directorio:
     if (exists(dir1) && is_directory(dir1)) {
-        i=0;
+    
+        //Se toma el tiempo:
+    chrono::high_resolution_clock::time_point ini = chrono::high_resolution_clock::now();
+    
+        //Llena el vector con todos los archivos del directorio:
         for (const auto& entry : directory_iterator(dir1)) {
-           v[i] = entry.path().string(); 
-           cout << v[i] << endl;
-           i=i+1;
-        } 
-    } 
-    else { 
-        // Handle the case where the directory doesn't exist 
-        cerr << "Directory not found." << endl; 
-        create_directory(dir1); 
-        cout << "directorio creado! " << dir1;
-        cout <<endl;
-
-    }
-    chrono::high_resolution_clock::time_point fin = chrono::high_resolution_clock::now();
-    // Calculamos duracion en segudos
-    auto duration_creation = chrono::duration_cast<chrono::seconds>(fin - ini);
-
-    // Imprimimos el tiempo de duracion
+           v.push_back(entry.path().string());
+        }
+        
+        // Calcula la duracion del proceso:
+        chrono::high_resolution_clock::time_point fin = chrono::high_resolution_clock::now();
+        auto duration_creation = std::chrono::duration_cast<std::chrono::seconds>(fin - ini);
+        
+        cout << "Vector creado!" <<endl;
+        // Imprimimos el tiempo de duracion
     printf("La creacion y ordenamiento del vector llevo %.4f segundos\n", static_cast<double>(duration_creation.count()));
-    cout << "Vector creado!" <<endl;
+        
+    }
+    //Si no existe, se avisa al usuario.
+    else { 
+        cout << "El directorio no existe." <<endl;
+    }
     return 0;
 }
